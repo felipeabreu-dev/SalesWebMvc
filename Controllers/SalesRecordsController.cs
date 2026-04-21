a6d38758931ce111192f5c90acc3c7d9d6d6d274
felipeabreu-dev <abreufelipe12@hotmail.com>
2026-04-21 17:36:09 -0300

    Preparing sales search navigation views

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}

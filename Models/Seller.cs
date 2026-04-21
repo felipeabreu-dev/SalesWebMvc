c47c122fb0ab01731aa66d382b2235bad5aa961f
felipeabreu-dev <abreufelipe12@hotmail.com>
2026-04-21 17:00:11 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -20,8 +20,12 @@ namespace SalesWebMvc.Models
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
@@ -30,6 +34,8 @@ namespace SalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100, 50000,ErrorMessage = "{0} must be {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
